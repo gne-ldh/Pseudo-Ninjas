4a1d120b940bffa7812c3ca3ad81d32d488327c1
root <root@localhost.localdomain>
2019-10-07 05:44:01 +0530

    Removing adminwars

--- Web/adminwars/dbconn.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $servername = "localhost";
-    $dbname = "ninjaart";
-    $dbuser = "root";
-    $dbpassword = "";
-
-    $conn = mysqli_connect($servername,$dbuser,$dbpassword,$dbname);
-?>
